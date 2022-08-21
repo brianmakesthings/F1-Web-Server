784fc6f678801d0f5f05e9596f6805cb9d9f2ffc
Brian Le <minh100@gmail.com>
2022-08-21 12:36:43 -0700

    redirect root to /analytics

--- f1_web_server/urls.py
+++ f1_web_server/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/analytics')),
     path('analytics/', include('f1_analytics.urls')),
     path('admin/', admin.site.urls),
 ]
