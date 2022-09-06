e7eba471fc06c7bdb545b79a8e52a90347897674
Brian Le <mla283@sfu.ca>
2022-09-06 16:56:02 -0700

    add compounds box and lineplot

--- f1_analytics/urls.py
+++ f1_analytics/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('lap_times', views.lap_times, name='lap_times'),
     path('tyre_usage', views.tyre_usage, name='tyre_usage'),
+    path('compounds_lineplot', views.compounds_lineplot, name='compounds_lineplot'),
+    path('compounds_boxplot', views.compounds_boxplot, name='compounds_boxplot'),
 
     # API endopoints
     path('events', views.events, name='events'),
--- f1_analytics/views.py
+++ f1_analytics/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from visualizations.visualizations import plot_driver_lap_times, tyre_usage_pie
 from f1_analytics.events_drivers import get_event_lists, event_to_drivers_csv
+from F1_Data_Visualization.visualization_funcs import plot_compounds_lineplot, plot_compounds_boxplot
 from io import StringIO
 
 # Create your views here.
@@ -10,6 +11,7 @@ def index(request):
 
 
 def lap_times(request):
+    template = "analytics/lap_times.html"
     form_data = {
         "year": request.GET.get("year"),
         "event": request.GET.get("event"),
@@ -23,7 +25,7 @@ def lap_times(request):
     }
     for key in form_data:
         if form_data[key] is None:
-            return render(request, "analytics/lap_times.html", context)
+            return render(request, template, context)
     lap_time_lineplot = plot_driver_lap_times(
         year=int(form_data["year"]),
         event=form_data["event"],
@@ -36,7 +38,7 @@ def lap_times(request):
     lap_time_lineplot.savefig(imgdata, format="svg")
     imgdata.seek(0)
     context["image"]= imgdata.getvalue()
-    return render(request, "analytics/lap_times.html", context)
+    return render(request, template, context)
 
 def tyre_usage(request):
     form_data = {
@@ -65,6 +67,71 @@ def tyre_usage(request):
     context["image"]= imgdata.getvalue()
     return render(request, "analytics/tyre_usage.html", context)
 
+def compounds_lineplot(request):
+    template = "analytics/compounds_lineplot.html"
+    form_data = {
+        "years": request.GET.getlist("years[]"),
+        "events": request.GET.getlist("events[]"),
+        "compounds": request.GET.getlist("compounds[]"),
+        "y-data": request.GET.get("y-data", "sLapTime"),
+        "x-data": request.GET.get("x-data", "TyreLife"),
+        "absolute_compound": request.GET.get("absolute_compound", False) == "on",
+    }
+    context = {
+        "image": "",
+        "compounds": ["SOFT", "MEDIUM", "HARD"],
+        "y_options": ["DeltaToRep", "DeltaToFastest", "PctFromRep", "PctFromFastest", "DeltaToLapRep", "PctFromLapRep", "sLapTime", "sDeltaToRep", "sDeltaToFastest", "sDeltaToLapRep"],
+        "x_options": ["TyreLife", "LapNumber"],
+    }
+    for key in form_data:
+        if form_data[key] is None or form_data[key] == []:
+            return render(request, template, context)
+    lap_time_lineplot = plot_compounds_lineplot(
+        years=[int(year) for year in form_data["years"]],
+        events=form_data["events"],
+        compounds=form_data["compounds"],
+        y=form_data["y-data"],
+        x=form_data["x-data"],
+        absolute_compound=form_data["absolute_compound"],
+    )
+    imgdata = StringIO()
+    lap_time_lineplot.savefig(imgdata, format="svg")
+    imgdata.seek(0)
+    context["image"]= imgdata.getvalue()
+    return render(request, template, context)
+
+def compounds_boxplot(request):
+    template = "analytics/compounds_boxplot.html"
+    form_data = {
+        "years": request.GET.getlist("years[]"),
+        "events": request.GET.getlist("events[]"),
+        "compounds": request.GET.getlist("compounds[]"),
+        "y-data": request.GET.get("y-data", "sLapTime"),
+        "x-data": request.GET.get("x-data", "TyreLife"),
+        "absolute_compound": request.GET.get("absolute_compound", False) == "on",
+    }
+    context = {
+        "image": "",
+        "compounds": ["SOFT", "MEDIUM", "HARD"],
+        "y_options": ["DeltaToRep", "DeltaToFastest", "PctFromRep", "PctFromFastest", "DeltaToLapRep", "PctFromLapRep", "sLapTime", "sDeltaToRep", "sDeltaToFastest", "sDeltaToLapRep"],
+        "x_options": ["TyreLife", "LapNumber"],
+    }
+    for key in form_data:
+        if form_data[key] is None or form_data[key] == []:
+            return render(request, template, context)
+    lap_time_lineplot = plot_compounds_boxplot(
+        years=[int(year) for year in form_data["years"]],
+        events=form_data["events"],
+        compounds=form_data["compounds"],
+        y=form_data["y-data"],
+        x=form_data["x-data"],
+        absolute_compound=form_data["absolute_compound"],
+    )
+    imgdata = StringIO()
+    lap_time_lineplot.savefig(imgdata, format="svg")
+    imgdata.seek(0)
+    context["image"]= imgdata.getvalue()
+    return render(request, template, context)
 
 def events(request):
     year = request.GET.get("year")
